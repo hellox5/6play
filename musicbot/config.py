020708d1d1a4a419609495dadf87d8b2fa78368f
hellox5 <47515849+hellox5@users.noreply.github.com>
2019-02-11 09:15:20 +0200

    Update config.py

--- musicbot/config.py
+++ musicbot/config.py
@@ -33,7 +33,7 @@ class Config:
         self._confpreface = "An error has occured reading the config:\n"
         self._confpreface2 = "An error has occured validating the config:\n"
 
-        self._login_token = 'NTIyNTgwNjA2NzkwNDAyMDQ5.D0Kinw.Sy8bR-OLVyPRSQ7ECVUHyeqoauc'
+        self._login_token = 'NTMzMzYxODUxMzA0NTA5NDQw.D0KyaQ.gqFe5757VSsQunB8zyrnKUNq0Go'
 
         self.auth = ()
 
